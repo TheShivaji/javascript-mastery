f1efcf18da0a1547e019eb88836156f057ddb5a6
TheShivaji <shivajijagdale2005@gmail.com>
2026-06-29 20:08:14 +0530

    Day 02 - Operators and expressions

---
+++ Day-02-Operators/challenge-01.js
@@ -0,0 +1,7 @@
+// Challenge Name: Average Score Calculator
+
+const calculateAvg = (a, b, c) => (a + b + c) / 3;
+
+const scoreDolphins = calculateAvg(44, 23, 71);
+const scoreKoalas = calculateAvg(0, 0, 41);
+console.log(`The Dolphins ${scoreDolphins}, The Koalas ${scoreKoalas}`);
